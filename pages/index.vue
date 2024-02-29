68774a7c8d5c888838209e600ec4f3c1785e6ba8
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-02-29 17:21:48 +0100

    add

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
     <main class="p-4 h-[200px] flex">
       <!-- Your page content goes here -->
       <p class="text-center text-2xl m-auto">Welcome to my Nuxt 3 app!</p>
