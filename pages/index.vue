d7c9997c03f5269be3c81b764d390dc64ac2841f
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-03-01 10:01:29 +0100

    dez

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
     <main class="p-4 h-[200px] flex">
       <!-- Your page content goes here -->
       <p class="text-center text-2xl m-auto">Welcome to my Nuxt 3 app!</p>
     </main>
   </div>
+
+  <script setup>
+    useHead({
+      script: [
+        { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+      ],
+    });
+
+  </script>
 </template>
 
