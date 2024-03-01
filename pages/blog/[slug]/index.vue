4a894d5ea036439652c99800e0a6ce1551a28576
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-03-01 14:09:36 +0100

    fdsq

--- pages/blog/[slug]/index.vue
+++ pages/blog/[slug]/index.vue
@@ -5,9 +5,10 @@ const slug = params.slug;
 const post = await queryContent(`/blog/${slug}`).findOne();
 </script>
 <template>
-    <v-container>
-        <h2 class="mb-4">{{ post.title }}</h2>
-        <ContentRenderer :value="post" />
-        <br />
+    fre
+    <v-container>qqqq
+        <h2 class="mb-4">{{ post.title }}</h2>ffff
+        <ContentRenderer :value="post" />dddd
+        <br>vvv
     </v-container>
 </template>
